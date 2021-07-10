941ffbbb9832e29602fd96281507b7c185a720f8
anderp2 <87140754+anderp2@users.noreply.github.com>
2021-07-10 11:57:57 -0400

    Update terraform_deploy.sh

--- terraform_deploy.sh
+++ terraform_deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-export ARM_CLIENT_ID="aede425f-2520-4579-baad-e88159a35699"
-export ARM_CLIENT_SECRET="d0zRtAR-.Do~uT40~7tGnd_zXD-Z6pdwAT"
-export ARM_SUBSCRIPTION_ID="24249cfa-81f0-427c-932d-621edd58f968"
-export ARM_TENANT_ID="a6ba6df7-9bcc-4dfc-85e4-8f2bd5594c7e"
+### Expecting input from TF_ variables for Service Principal
 
 cd /home/adminuser/git/iac-coding-challenge/terraform/deployment/
 terraform init
